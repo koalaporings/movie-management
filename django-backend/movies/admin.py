8ee314f2ed18999e10a8c12982554e7bf87793eb
Porings . <kylemanayon@gmail.com>
2025-07-29 16:11:33 +0800

    [backend] add movies model into admin

--- django-backend/movies/admin.py
+++ django-backend/movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'date_added')
+    search_fields = ('title',)
+
+admin.site.register(Movie, MovieAdmin)
