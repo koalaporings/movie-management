7ef914377ebed9c013725641d573d77fb20a9482
Porings . <kylemanayon@gmail.com>
2025-07-29 15:33:45 +0800

    [integration] fix routing

--- vue-frontend/src/router/index.js
+++ vue-frontend/src/router/index.js
@@ -1,14 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import App from '@/App.vue'
+import HomePage from '@/pages/HomePage.vue'
+import UploadPage from '@/pages/UploadPage.vue'
+import VideoPlayerPage from '@/pages/VideoPlayerPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
-      component: App,
+      component: HomePage,
     },
+    {
+      path: '/upload',
+      name: 'upload',
+      component: UploadPage
+    },
+    {
+      path: '/watch',
+      name: 'watch',
+      component: VideoPlayerPage
+    },
+
   ],
 })
 
