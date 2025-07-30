6897d769d5ae4e2a80fe21d5b832687413874b9e
Porings <kylemanayon@gmail.com>
2025-07-31 05:41:17 +0800

    [backend] remove video and thumbnail files when deleting a movie

--- django-backend/movies/models.py
+++ django-backend/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 # Create your models here.
 class Movie(models.Model):
@@ -7,3 +8,14 @@ class Movie(models.Model):
     date_added = models.DateField(auto_now=True)
     video_file = models.FileField(upload_to="movies/")
     thumbnail = models.ImageField(upload_to="thumbnails/")
+
+    def delete(self, *args, **kwargs):
+        if self.video_file:
+            if os.path.isfile(self.video_file.path):
+                os.remove(self.video_file.path)
+
+        if self.thumbnail:
+            if os.path.isfile(self.thumbnail.path):
+                os.remove(self.thumbnail.path)
+
+        super().delete(*args, **kwargs)
