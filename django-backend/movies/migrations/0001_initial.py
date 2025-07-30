ad4c453f18742300fce709b580744ca8f2db145b
Porings <kylemanayon@gmail.com>
2025-07-30 17:31:54 +0800

    [backend] add a thumbnail field for movies

--- django-backend/movies/migrations/0001_initial.py
+++ django-backend/movies/migrations/0001_initial.py
@@ -17,8 +17,9 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=120)),
                 ('description', models.TextField(blank=True)),
-                ('date_added', models.DateTimeField(auto_now=True)),
+                ('date_added', models.DateField(auto_now=True)),
                 ('video_file', models.FileField(upload_to='movies/')),
+                ('thumbnail', models.FileField(upload_to='thumbnails/')),
             ],
         ),
     ]
--- django-backend/movies/models.py
+++ django-backend/movies/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True)
-    date_added = models.DateTimeField(auto_now=True)
+    date_added = models.DateField(auto_now=True)
     video_file = models.FileField(upload_to="movies/")
+    thumbnail = models.ImageField(upload_to="thumbnails/")
